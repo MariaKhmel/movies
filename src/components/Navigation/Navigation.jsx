ff9475cb634b7267b1010e42c2edefe6d7593721
Maria Khmel <mariakhmel8@gmail.com>
2025-02-28 23:43:23 +0200

    styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,24 @@
 import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+import clsx from "clsx";
 
 const Navigation = () => {
+  const buildClassLink = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
   return (
-    <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies"> Movies</NavLink>
-    </>
+    <header>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildClassLink}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={buildClassLink}>
+          {" "}
+          Movies
+        </NavLink>
+      </nav>
+    </header>
   );
 };
 

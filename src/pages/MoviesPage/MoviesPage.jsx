a72146586ee356831b3ccbbb58fd74f901cc3347
Maria Khmel <mariakhmel8@gmail.com>
2025-03-01 20:16:57 +0200

    fx

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
-  return <div>MoviesPage</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieSearchValue = searchParams.get("query") || "";
+
+  useEffect(() => {
+    if (movieSearchValue === null) {
+      return;
+    }
+
+    //move search by api
+  }, [movieSearchValue]);
+
+  return (
+    <main>
+      <form>
+        <input type="text" name="input" />
+        <button type="submit">Search</button>
+      </form>
+    </main>
+  );
 };
 
 export default MoviesPage;

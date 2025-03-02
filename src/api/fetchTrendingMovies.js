d9c8ce2be50fdf619637c21bf1917bf570a6f6f4
Maria Khmel <mariakhmel8@gmail.com>
2025-03-02 23:46:47 +0200

    api

--- src/api/fetchTrendingMovies.js
+++ src/api/fetchTrendingMovies.js
@@ -1,21 +1,20 @@
 const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGZjMzgzZTU5NzRlMmFjNTQ3YTZiNjU1ZDJkYTNiMCIsIm5iZiI6MTY5OTQ4NTM1NC42MTYsInN1YiI6IjY1NGMxNmFhNDFhNTYxMzM2YTI0OWZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._IOJlhxYcCifsMvMcRjCFRP4jv2Oe0nXKB0e0kdcs7Q";
 
-const baseUrl = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+const baseUrl = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
+
 const options = {
+  method: 'GET',
   headers: {
-    Authorization: API_KEY
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
   }
 }
+
 const fetchTrendingMovies = () => {
   return fetch(`${baseUrl}&page=1`, options)
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(console.error(=>cl);
-  )
+    .then(data => data)
+    .catch(error => console.log(error));
 }
-export default fetchTrendingMovies;
 
-// fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
-//   .then(res => res.json())
-//   .then(res => console.log(res))
-//   .catch(err => console.error(err));
+export default fetchTrendingMovies;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import fetchTrendingMovies from "../../api/fetchTrendingMovies";
+
 const HomePage = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const getTrendingMovies = async () => {
+      const movies = await fetchTrendingMovies();
+      setTrendingMovies(movies.results);
+    };
+
+    getTrendingMovies();
+  }, []);
+
   return (
     <>
       <div>HomePage</div>

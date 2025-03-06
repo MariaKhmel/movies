02f93fd73fbd669d5bbd3583dc376078d7987c40
Maria Khmel <mariakhmel8@gmail.com>
2025-03-06 23:44:02 +0200

    MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,19 @@
-const MovieDetailsPage = () => {
-  return <div>MovieDetailsPage</div>;
+import { NavLink } from "react-router-dom";
+
+const MovieDetailsPage = ({ movie }) => {
+  return (
+    <section>
+      <div>
+        <img />
+        <div></div>
+      </div>
+      <div>
+        <p>Additional information</p>
+        <NavLink>Cast</NavLink>
+        <NavLink>Reviews</NavLink>
+      </div>
+    </section>
+  );
 };
 
 export default MovieDetailsPage;

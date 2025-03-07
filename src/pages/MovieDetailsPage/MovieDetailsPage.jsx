4ab9896efccc697aeb6c5ffc021556887db248a8
Maria Khmel <mariakhmel8@gmail.com>
2025-03-07 23:31:31 +0200

    fx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
 
 const MovieDetailsPage = ({ movie }) => {
+  // const { title } = movie;
   return (
     <main>
       <div>
         <img />
-        <div></div>
+        <div>
+          {/* <p>{title}</p> */}
+          <p>User score : {}</p>
+        </div>
       </div>
       <div>
         <p>Additional information</p>

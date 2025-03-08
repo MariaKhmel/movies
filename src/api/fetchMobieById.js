366a78bd1bf5c30fecbef5fa6c879c699d276cb0
Maria Khmel <mariakhmel8@gmail.com>
2025-03-08 23:57:39 +0200

    fx

--- src/api/fetchMobieById.js
+++ src/api/fetchMobieById.js
@@ -1,3 +1,15 @@
-const fetchMovieById = id => {
-  return fetch("").then(res => res.json()).then(data => data)
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGZjMzgzZTU5NzRlMmFjNTQ3YTZiNjU1ZDJkYTNiMCIsIm5iZiI6MTY5OTQ4NTM1NC42MTYsInN1YiI6IjY1NGMxNmFhNDFhNTYxMzM2YTI0OWZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._IOJlhxYcCifsMvMcRjCFRP4jv2Oe0nXKB0e0kdcs7Q";
+
+const baseUrl = "https://api.themoviedb.org/3/search/movie";
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const fetchMovieById = movieId => {
+  return fetch(`${baseUrl}/movieId`, options).then(res => res.json()).then(data => data)
 }

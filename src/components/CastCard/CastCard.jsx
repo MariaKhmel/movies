36d3a0a3927f820f0f43271f70c56e7358af1208
Maria Khmel <mariakhmel8@gmail.com>
2025-04-17 23:00:43 +0300

    fx

--- src/components/CastCard/CastCard.jsx
+++ src/components/CastCard/CastCard.jsx
@@ -2,7 +2,7 @@ const CastCard = ({ castMember }) => {
   const { original_name, character, profile_path } = castMember;
   return (
     <div>
-      <img />
+      <img alt={original_name} />
       <p>{original_name}</p>
       <p>Character:{character}</p>
     </div>

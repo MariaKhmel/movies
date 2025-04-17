e2a1f56b89fcb4f315267a357214f56585b8202b
Maria Khmel <mariakhmel8@gmail.com>
2025-04-17 23:04:01 +0300

    fx

--- src/components/CastCard/CastCard.jsx
+++ src/components/CastCard/CastCard.jsx
@@ -1,10 +1,12 @@
 import { baseImgUrl, defaultPoster } from "../../helpers/detFullImgPath";
+import css from "../CastCard/CastCard.module.css";
 
 const CastCard = ({ castMember }) => {
   const { original_name, character, profile_path } = castMember;
   return (
     <div>
       <img
+        className={css.castImg}
         alt={original_name}
         src={`${baseImgUrl}${profile_path ?? defaultPoster}`}
       />

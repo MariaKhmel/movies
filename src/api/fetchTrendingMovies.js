f9efef4952e1cdf51e9945f2fc61a170c277ee24
Maria Khmel <mariakhmel8@gmail.com>
2025-04-19 23:42:15 +0300

    fx

--- src/api/fetchTrendingMovies.js
+++ src/api/fetchTrendingMovies.js
@@ -1,14 +1,4 @@
-const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGZjMzgzZTU5NzRlMmFjNTQ3YTZiNjU1ZDJkYTNiMCIsIm5iZiI6MTY5OTQ4NTM1NC42MTYsInN1YiI6IjY1NGMxNmFhNDFhNTYxMzM2YTI0OWZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._IOJlhxYcCifsMvMcRjCFRP4jv2Oe0nXKB0e0kdcs7Q";
-
-const baseUrl = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
-
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
+import { baseUrl, options } from "./apiConfig";
 
 const fetchTrendingMovies = () => {
   return fetch(`${baseUrl}&page=1`, options)

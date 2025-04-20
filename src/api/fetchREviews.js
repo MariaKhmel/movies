f35e65c4907696a8b4d53a841c3dcc257030ba0f
Maria Khmel <mariakhmel8@gmail.com>
2025-04-20 20:48:30 +0300

    fx

--- src/api/fetchREviews.js
+++ src/api/fetchREviews.js
@@ -2,7 +2,7 @@ import { baseUrl, options } from "./apiConfig";
 
 const fetchReviews = (movieId) => {
   return fetch(`${baseUrl}/${movieId}/reviews?language=en-US&page=1`, options)
-    .then(res => console.log(res))
+    .then(res => res.json())
 }
 
 export { fetchReviews };

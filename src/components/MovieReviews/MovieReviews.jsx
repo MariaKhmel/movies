d8ad27e94b53eb8a6dfaafc61e4e0f35b75c80ab
Maria Khmel <mariakhmel8@gmail.com>
2025-04-21 22:33:04 +0300

    fx

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchReviews } from "../../api/fetchREviews";
 
 const MovieReviews = () => {
   const { movieId } = useParams();
+  const [cast, setCast] = useState();
 
   useEffect(() => {
     const getMovieCast = async () => {
       const reviews = await fetchReviews(movieId);
-      console.log(reviews);
+      setCast(reviews.results);
     };
     getMovieCast();
-  }, []);
+  }, [movieId]);
 
-  return <div>MovieReviews</div>;
+  return (
+    <div>
+      <ul>
+        {cast.map((el) => (
+          <li key={el.id}></li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieReviews;
